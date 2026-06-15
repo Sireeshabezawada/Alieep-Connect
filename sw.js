fe7801d31437ad062743fbb1d802769b29cbd08e
SIREESHA BEZAWADA <68730584+Sireeshabezawada@users.noreply.github.com>
2026-06-15 09:19:42 -0500

    Refactor service worker for cleanup and unregistration
    Updated service worker to clean up old caches and unregister itself.

--- sw.js
+++ sw.js
@@ -1,32 +1,13 @@
-// ALIEEP Connect Service Worker v4.1
-const CACHE_NAME = 'alieep-connect-v41';
-const ASSETS = [
-  '/',
-  '/index.html'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .catch(() => {})
-  );
-  self.skipWaiting();
-});
-
+// ALIEEP Connect - Service Worker Cleanup
+// This file unregisters old service workers that were causing loading issues
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
-  self.clients.claim();
+  self.registration.unregister();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() =>
-      caches.match(e.request).then(r => r || caches.match('/index.html'))
-    )
-  );
-});
